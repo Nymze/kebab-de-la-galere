d5ef45ed62e2eb7cfe905d65a15a445f5a005c21
Arnaud <arnaud.meneroud@gmail.com>
2018-01-31 20:03:53 +0000

    ajout formulaire la galere

---
+++ app/controllers/corsairs_controller.rb
@@ -0,0 +1,52 @@
+class CorsairsController < ApplicationController
+
+#créé un nouveau formulaire pour faire un nouveau profile
+def new
+	@corsair = Corsair.new
+
+end 
+
+#affiche le profile une fois remplit 
+def show
+	@corsair = Corsair.find(params[:id])
+end
+
+#permet d'editer le profile
+def edit
+	 @corsair = Corsair.find(params[:id])
+end
+
+#genere un nouveau profile en fonction du parametre
+
+def create
+		@corsair = Corsair.new(corsair_params)
+		if @corsair.save
+		redirect_to @corsair
+		else render 'new'
+		end
+	end
+
+	#permet de réecrire le profile choisi
+
+  def update
+  	@corsair = Corsair.find(params[:id])
+		if @corsair.update(corsair_params)
+			redirect_to corsairs_path
+		else 
+			render 'edit'
+		end
+	end
+
+	#list les profile completés 
+
+def index
+	@corsairs = Corsair.all
+end
+
+private
+	def corsair_params
+  	params.require(:corsair).permit(:first_name, :age, :likeness)
+	end
+
+
+end
